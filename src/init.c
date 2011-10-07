15d4ec1c34520ca4674013dd4467831d4a4abcc0
Greg Finak <greg.finak@ircm.qc.ca>
2011-10-07 23:43:13 +0000

    Fixed compile error on pitt (hopefully)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowWorkspace@58892 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/init.c
+++ src/init.c
@@ -1,24 +1,26 @@
 #include <R.h>
 #include <R_ext/Rdynload.h>
 #include "ncdfFlow.h"
-static const R_CallMethodDef CallEntries[] = {
+
 #ifdef HAVE_NETCDFDEF
+static const R_CallMethodDef CallEntries[] = {
     {"ncdf_bitarray_set", (DL_FUNC)&ncdf_bitarray_set, 2},
     {"ncdf_bitarray_getSetBitPos", (DL_FUNC)&ncdf_bitarray_getSetBitPos, 1},
     {"ncdf_bitarray_getBitStatus", (DL_FUNC)&ncdf_bitarray_getBitStatus, 1},
     {"ncdf_bitarray_Flip", (DL_FUNC)&ncdf_bitarray_Flip, 1},
     {NULL, NULL, 0}
-#endif
 };
 
 void R_init_ncdfFlow(DllInfo *dll)
 {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     //R_useDynamicSymbols(dll, FALSE);
-#ifdef HAVE_NETCDFDEF
+//#ifdef HAVE_NETCDFDEF
 	R_RegisterCCallable("ncdfFlow","ncdf_bitarray_set", (DL_FUNC)&ncdf_bitarray_set);
 	R_RegisterCCallable("ncdfFlow","ncdf_bitarray_getSetBitPos",(DL_FUNC)&ncdf_bitarray_getSetBitPos);
 	R_RegisterCCallable("ncdfFlow","ncdf_bitarray_getBitStatus",(DL_FUNC)&ncdf_bitarray_getBitStatus);
 	R_RegisterCCallable("ncdfFlow","ncdf_bitarray_Flip",(DL_FUNC)&ncdf_bitarray_Flip);
-#endif
+//#endif
 }
+
+#endif
