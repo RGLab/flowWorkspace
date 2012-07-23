fe586580451ea0706adb70a97a7ba96b83747bde
mikejiang <wjiang2@fhcrc.org>
2012-07-23 13:40:34 -0700

    add until.hpp

---
+++ src/include/until.hpp
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+
+string trim(string str)
+{
+  string::size_type pos = str.find_last_not_of(' ');
+  if(pos != string::npos) {
+    str.erase(pos + 1);
+    pos = str.find_first_not_of(' ');
+    if(pos != string::npos) str.erase(0, pos);
+  }
+  else str.erase(str.begin(), str.end());
+  return str;
+}
