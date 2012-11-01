99f53d54276be3a6b9eca1910bd96df9d88ac20f
mikejiang <wjiang2@fhcrc.org>
2012-11-01 14:59:47 -0700

    merge the fix for add quadgate method from trunk

--- R/add-methods.R
+++ R/add-methods.R
@@ -163,10 +163,11 @@ setMethod("add",
 			#clock-wise from top left quadrant
 			if(is.null(names))
 				names <- matrix(c(sprintf("%s-%s+", desc[1], desc[2]),
-											sprintf("%s-%s-", desc[1], desc[2]),
-											sprintf("%s+%s+", desc[1], desc[2]),
-											sprintf("%s+%s-", desc[1], desc[2])),
-										ncol=2)
+									sprintf("%s+%s+", desc[1], desc[2]),
+									sprintf("%s+%s-", desc[1], desc[2]),
+									sprintf("%s-%s-", desc[1], desc[2])
+									),
+								ncol=2)
 			if(length(unique(names))!=4)
 				stop("names have to be four unique strings!")
 			unlist(lapply(1:4,function(i){
