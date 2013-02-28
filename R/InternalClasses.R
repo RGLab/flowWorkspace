20d28e4b4bd01f2f0e2b676071b89d2e8d15ca36
m.jiang <m.jiang@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-02-28 18:53:58 +0000

    merge from trunk
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowWorkspace@73824 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/InternalClasses.R
+++ R/InternalClasses.R
@@ -69,6 +69,7 @@ setClass("GatingSetList",contains="list")
 ## Constructor
 GatingSetList <- function(x)
 {
+    names(x)<-NULL#strip names from the list because rbind2 doesn't like it
 	flowCore:::checkClass(x, "list")
 	x <- new("GatingSetList", .Data=x)
 	return(x)
