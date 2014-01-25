d28b4f77a75d8e10b44efe77e0ce01903c9dac7a
mikejiang <wjiang2@fhcrc.org>
2014-01-24 18:46:31 -0800

    update test files

--- inst/tests/test-GatingSet-accessors.R
+++ inst/tests/test-GatingSet-accessors.R
@@ -30,7 +30,7 @@ test_that("getData ",{
       expect_equal(nrow(ncfs[[1]]), 14623)
       ncfs <- getData(gs, 14)
       expect_equal(nrow(ncfs[[1]]), 14623)
-      expect_is(gh_list[[1]], "GatingHierarchy");
+      expect_is(gs[[1]], "GatingHierarchy");
       
       
     })
@@ -67,74 +67,74 @@ test_that("sampleNames",{
       sampleNames(gs) <- sn
     })
 
-test_that("clone & rbind2",{
-      
-      gs_clone <- clone(gs)
-      expect_is(gs_clone, "GatingSet");
-      
-      #check data consistency
-      fs1 <- getData(gs)
-      fs2 <- getData(gs_clone)
-      expect_identical(fs1[[1]], fs2[[1]])
-      
-      orig_sn <- sampleNames(gs)
-      clone_sn <- sampleNames(gs_clone)
-      expect_identical(orig_sn, clone_sn)
-      
-      #check tree consistentcy
-      expect_identical(getNodes(gs[[1]]), getNodes(gs_clone[[1]]))
-      
-      #construct gslist to rbind2
-      sampleNames(gs_clone) <- "CytoTrol_CytoTrol_2.fcs"
-      gslist <- GatingSetList(list(gs, gs_clone))
-      expect_is(gslist, "GatingSetList");
-      gs <<- rbind2(gslist)
-      
-      new_samples <- sampleNames(gs) 
-      expect_identical(new_samples, c(orig_sn, clone_sn))
-      
-    })
-
-
-test_that("lapply ",{
-      
-      gh_list <- lapply(gs, function(x)x)
-      expect_is(gh_list, "list");
-      expect_equal(length(gh_list), 2);
-      
-      res <- lapply(gh_list,function(gh){
-        expect_is(gh, "GatingHierarchy")
-      })
-      
-    })
-
-
-
-test_that("pData ",{
-      
-      pd <- pData(gs)
-      expect_is(pd, "data.frame");
-      
-      new_pd <- pd
-      new_pd[, "PTID"] <- c("001", "002")
-      pData(gs) <- new_pd 
-      expect_identical(pData(gs), new_pd);
-      
-      #restore pdata
-      pData(gs) <- pd
-      expect_identical(pData(gs), pd);
-    })
-
-test_that("[ subsetting",{
-      
-      gs_sub <- gs[2]
-      expect_is(gs_sub, "GatingSet");
-      gs_sub <- gs["CytoTrol_CytoTrol_2.fcs"]
-      expect_is(gs_sub, "GatingSet");
-      #c data structure does not change     
-      expect_true(identical(gs@pointer,gs_sub@pointer));
-      
-      expect_equal(length(gs_sub), 1)
-    })
-
-
+#test_that("clone & rbind2",{
+#      
+#      gs_clone <- clone(gs)
+#      expect_is(gs_clone, "GatingSet");
+#      
+#      #check data consistency
+#      fs1 <- getData(gs)
+#      fs2 <- getData(gs_clone)
+#      expect_identical(fs1[[1]], fs2[[1]])
+#      
+#      orig_sn <- sampleNames(gs)
+#      clone_sn <- sampleNames(gs_clone)
+#      expect_identical(orig_sn, clone_sn)
+#      
+#      #check tree consistentcy
+#      expect_identical(getNodes(gs[[1]]), getNodes(gs_clone[[1]]))
+#      
+#      #construct gslist to rbind2
+#      sampleNames(gs_clone) <- "CytoTrol_CytoTrol_2.fcs"
+#      gslist <- GatingSetList(list(gs, gs_clone))
+#      expect_is(gslist, "GatingSetList");
+#      gs <<- rbind2(gslist)
+#      
+#      new_samples <- sampleNames(gs) 
+#      expect_identical(new_samples, c(orig_sn, clone_sn))
+#      
+#    })
+#
+#
+#test_that("lapply ",{
+#      
+#      gh_list <- lapply(gs, function(x)x)
+#      expect_is(gh_list, "list");
+#      expect_equal(length(gh_list), 2);
+#      
+#      res <- lapply(gh_list,function(gh){
+#        expect_is(gh, "GatingHierarchy")
+#      })
+#      
+#    })
+#
+#
+#
+#test_that("pData ",{
+#      
+#      pd <- pData(gs)
+#      expect_is(pd, "data.frame");
+#      
+#      new_pd <- pd
+#      new_pd[, "PTID"] <- c("001", "002")
+#      pData(gs) <- new_pd 
+#      expect_identical(pData(gs), new_pd);
+#      
+#      #restore pdata
+#      pData(gs) <- pd
+#      expect_identical(pData(gs), pd);
+#    })
+#
+#test_that("[ subsetting",{
+#      
+#      gs_sub <- gs[2]
+#      expect_is(gs_sub, "GatingSet");
+#      gs_sub <- gs["CytoTrol_CytoTrol_2.fcs"]
+#      expect_is(gs_sub, "GatingSet");
+#      #c data structure does not change     
+#      expect_true(identical(gs@pointer,gs_sub@pointer));
+#      
+#      expect_equal(length(gs_sub), 1)
+#    })
+#
+#
--- inst/tests/test-parseWorkspace.R
+++ inst/tests/test-parseWorkspace.R
@@ -1,21 +1,21 @@
-#context("Workspace Functions")
-#
-#wsfile <- list.files(dataDir, pattern="manual.xml",full=TRUE)
-#
-#ws <- openWorkspace(wsfile);
-#
-#test_that("can load xml workspace",
-#{
-#expect_that(ws, is_a("flowJoWorkspace"))
-#})
-#
-#
-#G<-try(parseWorkspace(ws,execute=TRUE,path=dataDir,name=1));
-#test_that("Can parse workspace",{
-#	expect_that(G,is_a("GatingSet"));
-#	expect_that(G[[1]],is_a("GatingHierarchy"));
-#})
-#
+context("Workspace Functions")
+
+wsfile <- list.files(dataDir, pattern="manual.xml",full=TRUE)
+
+ws <- openWorkspace(wsfile);
+
+test_that("can load xml workspace",
+{
+expect_that(ws, is_a("flowJoWorkspace"))
+})
+
+
+G<-try(parseWorkspace(ws,execute=TRUE,path=dataDir,name=1));
+test_that("Can parse workspace",{
+	expect_that(G,is_a("GatingSet"));
+	expect_that(G[[1]],is_a("GatingHierarchy"));
+})
+
 #test_that("Population proportions are valid",{
 #	expect_that(all(sapply(getPopStats(G[[1]])[,1],function(x)!is.nan(x))),is_true())
 #})
