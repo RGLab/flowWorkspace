0ac3ab50622cde4acd9ef03774a77ae50a6115f5
mikejiang <wjiang2@fhcrc.org>
2014-11-03 17:19:25 -0800

    resolve the conflicting macro 'Free' between R and protobuf

--- src/pb/GatingSet.pb.h
+++ src/pb/GatingSet.pb.h
@@ -22,7 +22,7 @@
 #include <google/protobuf/generated_message_util.h>
 #include <google/protobuf/message.h>
 #include <google/protobuf/repeated_field.h>
-#include <google/protobuf/extension_set.h>
+//#include <google/protobuf/extension_set.h> //RS.h has conflicting Free() macro with this header
 #include <google/protobuf/generated_enum_reflection.h>
 #include <google/protobuf/unknown_field_set.h>
 // @@protoc_insertion_point(includes)
