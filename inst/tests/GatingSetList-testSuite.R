5157bc1f9781610abfcea049797d6484af50fadd
m.jiang <m.jiang@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-01-30 22:44:17 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commits.
    Commit information:
    
    Commit id: d723f7a1498f9ed339ee8db2106abbfc6348e78a
    
        Merge branch 'trunk'
    
    Committed by: mikejiang
    Author Name: mikejiang
    Commit date: 2015-01-30 14:43:46 -0800
    Author date: 2015-01-30 14:43:46 -0800
    
    Commit id: e2244c7af13323c9538591469c1d785ecf191a2c
    
        add ignore.case option to getSingleCellExpression method.  #168
    
    Committed by: mikejiang
    Author Name: mikejiang
    Commit date: 2015-01-30 14:43:27 -0800
    Author date: 2015-01-30 14:24:30 -0800
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowWorkspace@98916 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/tests/GatingSetList-testSuite.R
+++ inst/tests/GatingSetList-testSuite.R
@@ -89,7 +89,7 @@ test_that("keyword", {
 
 test_that("getSingleCellExpression for COMPASS",{
       
-      thisRes <- getSingleCellExpression(gslist, c('CD8/38- DR+', 'CD8/CCR7- 45RA+') , list("CD8/38- DR+" = "CD38 APC", "CD8/CCR7- 45RA+" = "CCR7 PE")) 
+      thisRes <- getSingleCellExpression(gslist, c('CD8/38- DR+', 'CD8/CCR7- 45RA+') , map = list("CD8/38- DR+" = "CD38 APC", "CD8/CCR7- 45RA+" = "CCR7 PE")) 
       expectRes <- readRDS(file.path(resultDir, "getData_COMPASS_gs.rds"))
       expect_equivalent(thisRes,expectRes)
       
