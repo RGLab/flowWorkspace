d863be5ee8c1d3ce50187e6c1453ed77954a91a3
mikejiang <wjiang2@fhcrc.org>
2015-02-23 11:56:56 -0800

    add 3 BS headers that are missing from BH package

--- src/include/flowData.hpp
+++ src/include/flowData.hpp
@@ -21,20 +21,17 @@ using namespace Rcpp;
 #endif
 
 #include <boost/config.hpp>
-#include <boost/archive/tmpdir.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
 #include <boost/archive/xml_iarchive.hpp>
 #include <boost/archive/xml_oarchive.hpp>
-#include <boost/serialization/scoped_ptr.hpp>
 #include <boost/serialization/base_object.hpp>
 #include <boost/serialization/utility.hpp>
 #include <boost/serialization/list.hpp>
 #include <boost/serialization/map.hpp>
 #include <boost/serialization/vector.hpp>
-#include <boost/serialization/valarray.hpp>
 #include <boost/graph/adj_list_serialize.hpp>
 #include <boost/serialization/assume_abstract.hpp>
 #include <boost/serialization/split_member.hpp>
