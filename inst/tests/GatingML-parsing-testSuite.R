4bc2f19b4cb6a75bf36074a2382f1981380e7a28
Mike Jiang <wjiang2@fhcrc.org>
2016-01-08 22:14:55 +0000

    add parse.gatingML and merge.count to streamline the parsing and count checking
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowWorkspace@112366 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/tests/GatingML-parsing-testSuite.R
+++ inst/tests/GatingML-parsing-testSuite.R
@@ -2,40 +2,19 @@ context("parse gatingMLs exported from Cytobank ")
 
 path <- "~/rglab/workspace/flowWorkspace/wsTestSuite"
 
+
 test_that("gatingML-cytobank parsing: cytotrol tcell",{
   xmlfile <- system.file("extdata/cytotrol_tcell_cytobank.xml", package = "flowWorkspace")
-  g <- read.gatingML.cytobank(xmlfile)
   fcsFiles <- list.files(pattern = "CytoTrol", system.file("extdata", package = "flowWorkspaceData"), full = T)
-  fs <- read.ncdfFlowSet(fcsFiles)
-  
-  
-  ## Compensate the data with the compensation information stored in `graphGML` object
-  fs <- compensate(fs, g)
-  ## Extract transformation functions from `graphGML` and transform the data
-  trans <- getTransformations(g)
-  fs <- transform(fs, trans)
-  
-  
-  ## Construct the **GatingSet** and apply gates stored in `graphGML`
-  gs <- GatingSet(fs)
-  gating(g, gs)
+  gs <- parse.gatingML(xmlfile, fcsFiles)
   
   
   #' ## verify the stats are correct
-  
-  #' 
-  #load stats from cytobank
-  cytobank_counts <- read.csv(system.file("extdata/cytotrol_tcell_cytobank_counts.csv", package = "flowWorkspace"), skip = 7, stringsAsFactors = F)
-  #convert it to the same format
-  colnames(cytobank_counts)[1] <- "Population"
-  cytobank_counts <- plyr::arrange(cytobank_counts, Population)
-  
-  #load openCyto stats
-  openCyto_counts <- getPopStats(gs, statType = "count")
-  openCyto_counts <- reshape2::dcast(openCyto_counts, Population ~ name, value.var = "Count")
+  statsfile <- system.file("extdata/cytotrol_tcell_cytobank_counts.csv", package = "flowWorkspace")
+  dt_merged <- merge.counts(gs, statsfile, id.vars = "population")
   
   
-  expect_equal(cytobank_counts, openCyto_counts, tol = 5e-4)
+  expect_equal(dt_merged[, count.x], dt_merged[, count.y], tol = 5e-4)
   
   
   
@@ -44,46 +23,13 @@ test_that("gatingML-cytobank parsing: cytotrol tcell",{
 test_that("gatingML-cytobank parsing: Merck FirstExample",{
   thisPath <- file.path(path, "/gatingML/Merck/firstExample")
   xmlfile <- file.path(thisPath, "CytExp_10623_Gates_v5.xml")
-  g <- read.gatingML.cytobank(xmlfile)
   fcsFiles <- list.files(pattern = "\\.fcs", thisPath, full = T)
-  fs <- read.ncdfFlowSet(fcsFiles)
-  
-  fs <- compensate(fs, g)
-  
-  trans <- getTransformations(g)
-  fs <- transform(fs, trans)
-  
-  gs <- GatingSet(fs)
-  gating(g, gs)
+  gs <- parse.gatingML(xmlfile, fcsFiles[c(1,3,6)])
   
   ### Verify the stats are correct
-  #load stats from cytobank
-  cytobank_counts <- fread(file.path(thisPath,"population_counts.csv"), stringsAsFactors = FALSE)
-  # Melt the data
-  cytobank_counts_long <- melt(cytobank_counts, variable.name = "population", value.name = "count", id.vars = "FCS Filename")
-  # Change column names
-  setnames(cytobank_counts_long, c("FCS Filename"), c("fcs_filename"))
-  # Properly format the column names
-  cytobank_counts_long <- cytobank_counts_long[,population := gsub("_EventCounts", "", population)]
+  statsfile <- file.path(thisPath,"population_counts.csv")
+  dt_merged <- merge.counts(gs, statsfile)
   
-  
-  # extract the counts from our gating sets
-  #load openCyto stats
-  opencyto_counts <- getPopStats(gs, statType = "count")
-  setnames(opencyto_counts, names(opencyto_counts), c("fcs_filename", "population", "parent", "count", "parent_count"))
-  #drop the parent column for simplicity
-  opencyto_counts <- opencyto_counts[,.(fcs_filename, population, count)]
-  #Remove spaces in population names as cytobank removes them here
-  opencyto_counts <- opencyto_counts[, population := gsub(" ", "", population)]
-  
-  
-  
-  # merge the two data.tables
-  
-  # set key (fcs_filename, population)
-  setkey(opencyto_counts, fcs_filename, population)
-  setkey(cytobank_counts_long, fcs_filename, population)
-  dt_merged <- merge(opencyto_counts, cytobank_counts_long)
   unequaled <- dt_merged[count.x != count.y]
   expect_equal(nrow(unequaled), 0)
 })
@@ -91,33 +37,15 @@ test_that("gatingML-cytobank parsing: Merck FirstExample",{
 test_that("gatingML-cytobank parsing: Merck SecondExample",{
   thisPath <- file.path(path, "/gatingML/Merck/SecondExample")
   xmlfile <- file.path(thisPath, "CytExp_10624_Gates_v3.xml")
-  g <- read.gatingML.cytobank(xmlfile)
   fcsFiles <- list.files(pattern = "\\.fcs", thisPath, full = T)
-  fs <- read.ncdfFlowSet(fcsFiles)
   
-  fs <- compensate(fs, g)
-  
-  trans <- getTransformations(g)
-  fs <- transform(fs, trans)
-  
-  gs <- GatingSet(fs)
-  gating(g, gs)
+  gs <- parse.gatingML(xmlfile, fcsFiles[c(1,4,6,12)])
   
   ### Verify the stats are correct
-  #load stats from cytobank
-  cytobank_counts <- read.csv(file.path(thisPath, "secondExample.csv"), skip = 7, stringsAsFactors = F, check.names = F)
-  #convert it to the same format
-  colnames(cytobank_counts)[1] <- "Population"
-  cytobank_counts[["Population"]] <- gsub("_EventCounts", "", cytobank_counts[["Population"]])
-  cytobank_counts <- plyr::arrange(cytobank_counts, Population)
-  
-  #load openCyto stats
-  openCyto_counts <- getPopStats(gs, statType = "count")
-  openCyto_counts <- reshape2::dcast(openCyto_counts, Population ~ name, value.var = "Count")
-  openCyto_counts[["Population"]] <- gsub(" ", "", openCyto_counts[["Population"]])
-  #compare two
-  expect_equal(cytobank_counts, openCyto_counts)
+  statsfile <- file.path(thisPath,"secondExample.csv")
+  dt_merged <- merge.counts(gs, statsfile, id.vars = "population") #subset the files to speed up testing
   
   
-  
+  unequaled <- dt_merged[count.x != count.y]
+  expect_equal(nrow(unequaled), 0)
 })
