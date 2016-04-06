48266af26737590f78246da331409ae1107f1188
Mike Jiang <wjiang2@fhcrc.org>
2016-04-06 00:24:26 +0000

    expose pb lib for GatingSetLoader to link against. #179
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowWorkspace@115849 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,8 @@
 # copied from plyr to avoid the dependency on plyr
 compact <- function (l) 
   Filter(Negate(is.null), l)
+
+LdFlags <- function(){
+  libpath <- paste0("lib", Sys.getenv("R_ARCH"), "/libprotobuf.a")
+  cat(tools::file_path_as_absolute( base::system.file(libpath, package = "flowWorkspace" )))
+}
