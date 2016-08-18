2b35bdc2541c24412389a16b5cdae39d5ee3acaa
mikejiang <wjiang2@fhcrc.org>
2016-08-18 14:25:15 -0700

    move inst/tests to tests/testthat/

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,12 +1,9 @@
 library(testthat)
 library(flowWorkspace)
-library(data.table)
-library(utils)
-library(dplyr)
 
-dataDir <- system.file("extdata",package="flowWorkspaceData")
-resultDir <- system.file("tests/expect_result",package="flowWorkspace")
-test_package("flowWorkspace")
+test_check("flowWorkspace")
+
+#devtools::test("~/rglab/workspace/flowWorkspace")
 
 #test_file("/home/wjiang2/rglab/workspace/flowWorkspace/inst/tests/test-archive.R")
 #test_file("/home/wjiang2/rglab/workspace/flowWorkspace/inst/tests/test-parseWorkspace.R")
---
+++ tests/testthat/helper-expectResult.R
@@ -0,0 +1,6 @@
+library(data.table)
+library(utils)
+library(dplyr)
+
+dataDir <- system.file("extdata",package="flowWorkspaceData")
+resultDir <- "expect_result"
