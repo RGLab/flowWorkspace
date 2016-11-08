678e43eb799be95031f4ee94508559b989e7f401
mikejiang <wjiang2@fhcrc.org>
2016-11-08 15:33:10 -0800

    expose pb header and static lib for linking

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,10 @@
 compact <- function (l) 
   Filter(Negate(is.null), l)
 
-LdFlags <- function(){
-  libpath <- paste0("lib", Sys.getenv("R_ARCH"), "/libprotobuf.a")
-  cat(tools::file_path_as_absolute( base::system.file(libpath, package = "flowWorkspace" )))
+LdFlags <- function(lib=c("pb", "all")){
+  lib <- match.arg(lib)
+  
+  libpaths <- paste0("lib", Sys.getenv("R_ARCH"), if(lib == "pb") "/libprotobuf.a" else c("/libflowWorkspace.a", "/libprotobuf.a"))
+  libpaths <- lapply(libpaths, function(libpath)tools::file_path_as_absolute( base::system.file(libpath, package = "flowWorkspace" )))
+  cat(paste(libpaths, collapse = " "))
 }
