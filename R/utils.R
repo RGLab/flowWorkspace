de5cdca78aa4db82fcdbd476c07c5204c6c71d4d
Mike Jiang <wjiang2@fhcrc.org>
2017-05-17 22:01:39 +0000

    Preserve sampleName in merged stats
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowWorkspace@129671 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,17 @@
 # copied from plyr to avoid the dependency on plyr
-compact <- function (l) 
+compact <- function (l)
   Filter(Negate(is.null), l)
 
 LdFlags <- function(lib=c("pb", "flowWorkspace", "all")){
   lib <- match.arg(lib)
   libs <- c("/libflowWorkspace.a", "/libprotobuf.a")
-  if(lib == "pb") 
+  if(lib == "pb")
     lib <- libs[2]
   else if(lib == "flowWorkspace")
     lib <- libs[1]
   else
     lib <- libs
-    
+
   libpaths <- paste0("lib", Sys.getenv("R_ARCH"), lib)
   libpaths <- lapply(libpaths, function(libpath)tools::file_path_as_absolute(base::system.file(libpath, package = "flowWorkspace" )))
   cat(paste(libpaths, collapse = " "))
@@ -38,6 +38,7 @@ getMergedStats = function(object,...){
 	stats = getPopStats(object,...)
 	#process name column so that it contains XXX.fcs
 	message("Processing sample names..")
+	stats[, sampleName:=name]
 	stats[,name := gsub("(fcs).*","\\1",name)]
 	pd = pData(object)
 	message("merging..")
@@ -45,3 +46,4 @@ getMergedStats = function(object,...){
 	message("Done")
 	return(ret)
 }
+
