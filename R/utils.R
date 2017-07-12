4730f22ba48292e4feb41e1e99307253a4bf58a7
mikejiang <wjiang2@fhcrc.org>
2017-07-12 16:46:19 -0700

    update LdFlags function

--- R/utils.R
+++ R/utils.R
@@ -2,16 +2,10 @@
 compact <- function (l)
   Filter(Negate(is.null), l)
 
-LdFlags <- function(lib=c("pb", "flowWorkspace", "all")){
-  lib <- match.arg(lib)
-  libs <- c("/libflowWorkspace.a", "/libprotobuf.a")
-  if(lib == "pb")
-    lib <- libs[2]
-  else if(lib == "flowWorkspace")
-    lib <- libs[1]
-  else
-    lib <- libs
-
+LdFlags <- function(){
+ 
+  lib <- "/libflowWorkspace.a"
+    
   libpaths <- paste0("lib", Sys.getenv("R_ARCH"), lib)
   libpaths <- lapply(libpaths, function(libpath)tools::file_path_as_absolute(base::system.file(libpath, package = "flowWorkspace" )))
   cat(paste(libpaths, collapse = " "))
