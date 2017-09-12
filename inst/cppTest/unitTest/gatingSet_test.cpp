4d63b53d052ab2c253c9f56de377b3cbb21d1616
mikejiang <wjiang2@fhcrc.org>
2017-09-12 14:15:18 -0700

    write time log for unit test

--- inst/cppTest/unitTest/gatingSet_test.cpp
+++ inst/cppTest/unitTest/gatingSet_test.cpp
@@ -316,11 +316,26 @@ void parser_test(testCase & myTest){
 		 */
 		if(isLoadArchive)
 		{
+			std::clock_t    start;
+
+			 start = std::clock();
 			flowData res=nc.readflowData(curSample);
 			gh->loadData(res);//
 			gh->transforming(1);
 			gh->gating(0,true);
-			gh->unloadData();;
+			gh->unloadData();
+#ifdef PRT
+			delete res.data;
+			string filename = "timelog2";
+#else
+			string filename = "timelog1";
+#endif
+			double runtime = (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000);
+			ofstream timelog;
+			timelog.open(filename.c_str(), ios_base::app);
+//			myTest.times.push_back(runtime);
+			timelog << runtime << ",";
+			timelog.close();
 		}
 
 		gh_counts(gh, myTest.isEqual, myTest.tolerance, myTest.skipPops);
