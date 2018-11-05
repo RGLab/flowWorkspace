f4befd7fffcc06edc84609d2f7de2396b6d780ae
mikejiang <wjiang2@fhcrc.org>
2018-11-05 10:53:48 -0800

    merge swap_data_col commit

--- R/GatingSet_Methods.R
+++ R/GatingSet_Methods.R
@@ -1,5 +1,326 @@
 #' @include GatingHierarchy_Methods.R
 NULL
+
+#' determine the flow data associated with a Gating Hiearchy is based on `ncdfFlowSet` or `flowSet`
+#'
+#' @param x \code{GatingHiearchy} object
+#' @return \code{logical}
+#' @export
+isNcdf <- function(x){
+#			browser()
+
+  return (class(flowData(x))=="ncdfFlowSet")
+
+}
+
+#' @title save/load a GatingSet/GatingSetList to/from disk.
+#'
+#' @description
+#' Save/load a GatingSet/GatingSetList which is the gated flow data including gates and populations to/from the disk.
+#' The \code{GatingSet} object The internal C data structure (gating tree),\code{ncdfFlowSet} object(if applicable)
+#'
+#' @param G A \code{GatingSet}
+#' @param gslist A \code{GatingSetList}
+#' @param path A character scalar giving the path to save/load the GatingSet to/from.
+#' @param overwrite A logical scalar specifying whether to overwrite the existing folder.
+#' @param cdf a character scalar. The valid options are :"copy","move","skip","symlink","link" specifying what to do with the cdf data file.
+#'              Sometime it is more efficient to move or create a link of the existing cdf file to the archived folder.
+#'              It is useful to "skip" archiving cdf file if raw data has not been changed.
+#' @param ... other arguments: not used.
+#'
+#'
+#' @return
+#' \code{load_gs} returns a GatingSet object
+#' \code{load_gslist} returns a GatingSetList object
+#'
+#' @seealso \code{\link{GatingSet-class}},\code{\link{GatingSetList-class}}
+#'
+#' @examples
+#' \dontrun{
+#' 	#G is a GatingSet
+#' 	save_gs(G,path="tempFolder")
+#' 	G1<-load_gs(path="tempFolder")
+#'
+#' 	#G is a GatingSet
+#'
+#' 	save_gslist(gslist1,path="tempFolder")
+#' 	gslist2<-load_gslist(path="tempFolder")
+#' }
+#' @rdname save_gs
+#' @export
+#' @aliases save_gs load_gs save_gslist load_gslist
+save_gs<-function(G,path,overwrite = FALSE
+                    , cdf = c("copy","move","skip","symlink","link")
+                    , ...){
+#  browser()
+  cdf <- match.arg(cdf)
+  fileext <- 'pb'
+
+
+  guid <- G@guid
+  if(length(guid) == 0){
+    G@guid <- .uuid_gen()
+    guid <- G@guid
+  }
+  rds_toSave <- paste(guid,"rds",sep=".")
+  dat_toSave <- paste(guid,fileext,sep=".")
+
+  if(file.exists(path)){
+    path <- normalizePath(path,mustWork = TRUE)
+    if(overwrite){
+      this_files <- list.files(path)
+      #validity check for non-empty folder
+      if(length(this_files)!=0)
+      {
+        rds_ind <- grep("\\.rds$",this_files)
+        dat_ind <- grep(paste0("\\.",fileext,"$"),this_files)
+
+        if(length(rds_ind)!=1||length(dat_ind)!=1){
+          stop("Not a valid GatingSet archiving folder! ", path)
+        }else{
+          this_rds <- this_files[rds_ind]
+          this_dat <- this_files[dat_ind]
+
+          if(this_rds!=rds_toSave||this_dat!=dat_toSave){
+            stop("The GatingSet doesn't match the archived files in: ", path)
+          }
+        }
+      }
+
+      #validity check for cdf
+      if(isNcdf(G)){
+        if(length(this_files)!=0){
+          cdf_ind <- grep("\\.nc$",this_files)
+          if(length(cdf_ind) != 1){
+            stop("Not a valid GatingSet archiving folder! ", path)
+          }
+        }
+
+      }
+      if(length(this_files)!=0)
+      {
+        #start to delete the old files in path
+        file.remove(file.path(path,rds_toSave))
+        file.remove(file.path(path,dat_toSave))
+
+        if(isNcdf(G)){
+          #check if the target path is the same as current cdf path
+#            browser()
+          this_cdf <- file.path(path,this_files[cdf_ind])
+          if(normalizePath(getData(G)@file)==normalizePath(this_cdf)){
+            cdf <- "skip"
+          }
+          if(cdf != "skip"){
+            file.remove(this_cdf)
+          }
+        }
+      }
+
+    }else{
+      stop(path,"' already exists!")
+    }
+
+  }else{
+    dir.create(path = path)
+    #do the dir normalization again after it is created
+    path <- normalizePath(path,mustWork = TRUE)
+
+  }
+#  browser()
+  invisible(.save_gs(G=G,path = path, cdf = cdf, ...))
+  message("Done\nTo reload it, use 'load_gs' function\n")
+
+
+}
+
+
+#' @rdname save_gs
+#' @export
+#' @aliases load_gs load_gslist
+load_gs<-function(path){
+#  browser()
+  path <- normalizePath(path,mustWork = TRUE)
+  if(!file.exists(path))
+    stop(path,"' not found!")
+  files<-list.files(path)
+#   browser()
+  .load_gs(output = path, files = files)$gs
+
+}
+
+
+
+
+#' serialization functions to be called by wrapper APIs
+#' @noRd 
+.save_gs <- function(G,path, cdf = c("copy","move","skip","symlink","link")){
+
+#    browser()
+    cdf <- match.arg(cdf)
+    fileext <- 'pb'
+
+    if(!file.exists(path))
+      stop("Folder '",path, "' does not exist!")
+    #generate uuid for the legacy GatingSet Object
+    if(length(G@guid)==0){
+      G@guid <- .uuid_gen()
+    }
+    guid <- G@guid
+
+    rds.file<-file.path(path,paste(guid,"rds",sep="."))
+    dat.file<-file.path(path,paste(guid,fileext,sep="."))
+
+
+    filestoSave <- c(rds.file,dat.file)
+    #save ncdf file
+    if(cdf != "skip" && isNcdf(G))
+    {
+      from<-flowData(G)@file
+#      browser()
+      if(cdf == "move"){
+        message("moving ncdf...")
+        ncFile <- file.path(path,basename(from))
+        res <- file.rename(from,ncFile)
+        #reset the file path for ncdfFlowSet
+        flowData(G)@file <- ncFile
+      }else{
+
+        ncFile<-tempfile(tmpdir=path,fileext=".nc")
+
+        if(cdf == "copy"){
+          message("saving ncdf...")
+          res <- file.copy(from=from,to=ncFile)
+        }
+        else if(cdf == "symlink"){
+          message("creating the symbolic link to ncdf...")
+          res <- file.symlink(from=from,to=ncFile)
+        }else if(cdf == "link"){
+          message("creating the hard link to ncdf...")
+          res <- file.link(from=from,to=ncFile)
+        }
+      }
+      if(!res){
+        stop("failed to ",cdf," ",from,"!")
+      }
+      filestoSave<-c(filestoSave,ncFile)
+    }
+
+    message("saving tree object...")
+    #save external pointer object
+    .cpp_saveGatingSet(G@pointer, dat.file)
+
+    message("saving R object...")
+    saveRDS(G,rds.file)
+
+    filestoSave
+
+}
+#' unserialization functions to be called by wrapper APIs
+#' @importFrom tools file_ext
+#' @noRd 
+.load_gs <- function(output,files){
+      dat.file <- file.path(output,files[grep(".pb$|.dat$|.txt$|.xml$",files)])
+      rds.file<-file.path(output,files[grep(".rds$",files)])
+
+      nc.file<-file.path(output,files[grep(".nc$|.nc.trans$",files)])
+    #   browser()
+      if(length(dat.file)==0)
+        stop(".dat file missing in ",output)
+      if(length(dat.file)>1)
+        stop("multiple .dat or .pb files found in ",output)
+      fileext <- file_ext(dat.file)
+      isPB <- fileext == "pb"
+      if(!isPB)
+        stop("Legacy 'Boost serialization' format is detected. Try to convert it with 'boost2protobuf' package!")
+      if(length(rds.file)==0)
+        stop(".rds file missing in ",output)
+      if(length(rds.file)>1)
+        stop("multiple .rds files found in ",output)
+
+      message("loading R object...")
+      gs <- readRDS(rds.file)
+
+      #deal with legacy archive
+      if(class(gs) == "GatingSetInternal")
+      {
+        thisSet <- attr(gs,"set")
+        thisGuid <- attr(gs,"guid")
+        if(is.null(thisGuid))
+          thisGuid <- .uuid_gen()
+#        browser()
+        thisGH <- thisSet[[1]]
+        thisTree <- attr(thisGH, "tree")
+        thisPath <- dirname(attr(thisGH, "dataPath"))
+        thisData <- graph::nodeDataDefaults(thisTree)[["data"]]
+        fs <- thisData[["data"]][["ncfs"]]
+
+        axis <- sapply(thisSet, function(gh){
+                            thisTree <- attr(thisGH, "tree")
+                            thisData <- graph::nodeDataDefaults(thisTree)[["data"]]
+#                            browser()
+                            thisSample <- attr(thisGH, "name")
+                            thisChnls <- colnames(fs@frames[[thisSample]])
+                            thisAxis <- thisData[["axis.labels"]]
+                            if(is.null(thisAxis))
+                              list()
+                            else{
+                              names(thisAxis) <- thisChnls
+                              thisAxis
+                            }
+
+                          }, simplify = FALSE)
+
+
+        gs <- new("GatingSet", flag = TRUE, guid = thisGuid, axis = axis, data = fs)
+      }
+
+      if(!.hasSlot(gs, "transformation"))
+        gs@transformation <- list()
+
+      if(!.hasSlot(gs, "compensation"))
+        gs@compensation <- NULL
+      
+      guid <- try(slot(gs,"guid"),silent=T)
+      if(class(guid)=="try-error"){
+        #generate the guid for the legacy archive
+        gs@guid <- .uuid_gen()
+      }
+
+      message("loading tree object...")
+      gs@pointer<-.cpp_loadGatingSet(dat.file)
+
+
+      if(isNcdf(gs))
+      {
+        if(length(nc.file)==0)
+          stop(".nc file missing in ",output)
+        flowData(gs)@file <- nc.file
+
+      }
+      #deal with legacy gs that stores single comp in compensation slot
+      comp <- gs@compensation
+      if(!is.null(comp))
+      {
+        if(!is.list(comp)||is.data.frame(comp)){
+          gs@compensation <- sapply(sampleNames(gs), function(sn)comp, simplify = FALSE)
+        }
+         
+      }
+      trans <- gs@transformation
+      if(length(trans)!=0)
+      {
+        if(is(trans , "transformerList")){
+          gs@transformation <- sapply(sampleNames(gs), function(sn)trans, simplify = FALSE)
+        }
+        
+      }
+      
+      message("Done")
+      list(gs=gs,files=c(dat.file,rds.file))
+}
+
+
+
 #' constructors for GatingSet
 #'
 #' construct object from existing gating hierarchy(gating template) and flow data 
@@ -51,13 +372,55 @@ setMethod("GatingSet", c("GatingHierarchy", "character"), function(x, y, path=".
 			return(gs)
 		})
 
-
 setMethod("identifier",
           signature=signature(object="GatingSet"),
           definition=function (object)
           {
             get_gatingset_id(object@pointer)
           })
+#' Swap the colnames
+#' Perform some validity checks before returning the updated colnames
+#'  
+#' @param cols the original colname vector
+#' @param swap_cols a named list specifying the pairs to be swapped
+#' @return the new colname vector that has some colnames swapped
+#' @export 
+#' @examples 
+#' data(GvHD)
+#' fr <- GvHD[[1]]
+#' colnames(fr)
+#' new <- swap_data_cols(colnames(fr), list(`FSC-H` = "SSC-H", `FL2-H` = "FL2-A"))
+#' colnames(fr) <- new
+swap_data_cols <- function(cols, swap_cols)
+{
+	if(!is.null(swap_cols))
+		if(!isFALSE(swap_cols) && length(swap_cols) > 0) 
+		{
+			left <- names(swap_cols)
+			right <- as.vector(unlist(swap_cols))
+			
+			update <- FALSE
+			for(left in names(swap_cols))
+			{
+				right <- swap_cols[[left]]
+				
+				lidx <- match(left, cols)
+				ridx <- match(right, cols)
+				
+				if(!is.na(lidx) && !is.na(ridx))
+				{
+					if(length(lidx) > 1) 
+						stop("Multiple cols matched to ", left)
+					if(length(ridx) > 1)
+						stop("Multiple cols matched to ", right)
+					message("swap cols: ", left, ":", right)  
+					
+					cols[c(lidx, ridx)]  <- c(right, left)
+				}
+			}
+		}
+	cols
+}
 
 setReplaceMethod("identifier",
                  signature=signature(object="GatingSet"),
@@ -78,12 +441,15 @@ setReplaceMethod("identifier",
 #'                                  But when the gates needs to be extended without loading the raw data (i.e. \code{execute} is set to FALSE), then this hard-coded value is used.
 #' @param transform \code{logical} to enable/disable transformation of gates and data. Default is TRUE. It is mainly for debug purpose (when the raw gates need to be parsed.
 #' @importFrom Biobase AnnotatedDataFrame
+#' @noRd 
 .addGatingHierarchies <- function(gs, samples, execute,isNcdf = TRUE
                                       ,compensation=NULL,wsType = ""
                                       , extend_val = 0, extend_to = -4000
                                       , prefix = TRUE, channel.ignore.case = FALSE
                                       , ws = NULL, leaf.bool = TRUE, sampNloc = "keyword"
-                                      ,  transform = TRUE, timestep.source = c("TIMESTEP", "BTIM"), ...){
+                                      ,  transform = TRUE, timestep.source = c("TIMESTEP", "BTIM")
+									  , swap_cols = FALSE #for diva parsing
+							  , ...){
   timestep.source  <- match.arg(timestep.source )
   if(nrow(samples)==0)
     stop("no sample to be added to GatingSet!")
@@ -172,7 +538,10 @@ setReplaceMethod("identifier",
 				data <- read.FCS(file, ...)[, cnd]
 			else
 				data <- fs[[guid]]
-
+			
+		cols <- swap_data_cols(colnames(data), swap_cols)
+		if(!all(cols==colnames(data)))
+			colnames(data) <- cols
       #alter colnames(replace "/" with "_") for flowJo X
       #record the locations where '/' character is detected and will be used to restore it accurately
       slash_loc <- sapply(cnd, function(thisCol)as.integer(gregexpr("/", thisCol)[[1]]), simplify = FALSE)
@@ -547,6 +916,7 @@ fix_channel_slash <- function(chnls, slash_loc = NULL){
 #'
 #' @return
 #' a \code{list} of axis labels and positions. Also, the \code{range} slot of \code{flowFrame} stored in \code{frmEnv} are transformed as an side effect.
+#' @noRd 
 .transformRange <- function(G,sampleName, wsType,frmEnv, timeRange = NULL, slash_loc = NULL, compChnlInd){
 
 
@@ -742,6 +1112,7 @@ setMethod("plotGate",signature(x="GatingSet",y="character"),function(x,y,lattice
     })
 
 ##recursively parsing conditional variables
+#' @noRd 
 .parseCond<-function(cond){
 #			browser()
   groupBy<-NULL
@@ -830,6 +1201,7 @@ setMethod("plotGate",signature(x="GatingSet",y="character"),function(x,y,lattice
 #' @param type \code{character} either 'xyplot' or 'densityplot'
 #' @param stats \code{numeric} proportions of cell population. If \code{missing} then extract it from \code{gatingHiearchy}
 #' @return a \code{list} containing 'gates', 'xParam','yParam', and 'stats'
+#' @noRd 
 .preplot <- function(x, y, type, stats, formula, default.x = "FSC-A", default.y = "SSC-A"){
   samples <- sampleNames(x)
 
@@ -955,6 +1327,7 @@ fix_y_axis <- function(gs, x, y){
 #' @param ... other arguments
 #'      params channel names for subsetting the result
 #' @return either a list of flowFrame for a flowSet/ncdfFlowSet or NULL
+#' @noRd 
 .getOverlay <- function(x, overlay, ...){
   .Defunct(msg = "getOverlay is defunct!")
   myfunc <- function(x, overlay, params){
@@ -994,6 +1367,7 @@ fix_y_axis <- function(gs, x, y){
 #' @param fs \code{flowSet} or \code{ncdfFlowSet}
 #' @param channel \code{character} channel name
 #' @return a numerical range
+#' @noRd 
 .getRange <- function(fs, channel) {
 
   thisMin <- .Machine$double.xmax
@@ -1020,6 +1394,7 @@ fix_y_axis <- function(gs, x, y){
 #' @param ... other arguments passed to .formAxis and flowViz
 #' @importMethodsFrom flowViz xyplot densityplot
 #' @importFrom RColorBrewer brewer.pal
+#' @noRd 
 .plotGate <- function(x, y, formula=NULL, cond=NULL
                       , smooth=FALSE ,type = flowWorkspace.par.get("plotGate")[["type"]]
                       , main = NULL
@@ -1350,6 +1725,7 @@ setMethod("recompute",c("GatingSet"),function(x, y="root",alwaysLoadData=FALSE, 
 #'                  In that case, we allow the gating to be failed and prompt user to recompute those nodes explictily
 #'                  When TRUE, then it forces data to be loaded to guarantee the gating process to be uninterrupted
 #'                  , yet may at the cost of unnecessary data IO
+#' @noRd 
 .recompute <- function(x,y = "root", alwaysLoadData = FALSE, verbose = FALSE, leaf.bool = TRUE){
   cpp_gating(x@pointer, y, alwaysLoadData, verbose, leaf.bool)
   
@@ -1750,6 +2126,7 @@ setMethod("getPopStats", "GatingSet",
 })
 
 #' calculate the coefficient of variation
+#' @noRd 
 .computeCV <- function(x, ...){
 
   #columns are populations
