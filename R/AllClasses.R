7345e44d0a93e92be403598f863d34e5f5029432
mikejiang <wjiang2@fhcrc.org>
2019-01-03 16:07:50 -0800

    link dynamically to RProtobufLib

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -11,6 +11,7 @@ NULL
 #' flowWorkspace
 #' @docType package
 #' @importFrom Rcpp evalCpp
+#' @importFrom RProtoBufLib LdFlags
 #' @title Import and replicate flowJo workspaces and gating schemes using flowCore.
 #' @description Import flowJo workspaces into R. Generate the flowJo gating hierarchy and gates using flowCore functionality. Transform and compensate data in accordance with flowJo settings. Plot gates, gating hierarchies,  population statistics, and compare flowJo vs flowCore population summaries.
 #' @details
