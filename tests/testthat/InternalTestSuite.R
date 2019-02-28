7041a23b863524f202700e7c7f034f9f3b4dd7b2
mikejiang <wjiang2@fhcrc.org>
2019-02-28 14:06:31 -0800

    fix test error

--- tests/testthat/InternalTestSuite.R
+++ tests/testthat/InternalTestSuite.R
@@ -21,8 +21,8 @@ test_that("set T value properly through PnE instead of PnR for flog transform wh
   
   wsFile <- file.path(path, "flog_PnE/Liver.wsp")
   
-  ws <- openWorkspace(wsFile)
-  gs <- parseWorkspace(ws, name = 2, sampNloc = 'sampleNode')
+  ws <- openWorkspace(wsFile, sampNloc = 'sampleNode')
+  gs <- parseWorkspace(ws, name = 2)
   
   res <- getPopStats(gs[[1]])
   expect_equal(res[, xml.freq], res[, openCyto.freq], tol = 0.015)
