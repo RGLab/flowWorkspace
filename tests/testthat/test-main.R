5000e1cbfa76a856d1ab9a52fbeaf565cbb90e85
mikejiang <wjiang2@fhcrc.org>
2020-06-25 09:52:34 -0700

    conditional add test cases

--- tests/testthat/test-main.R
+++ tests/testthat/test-main.R
@@ -33,7 +33,15 @@ source("cleanup_temp.R", local = TRUE)
 source("convert-legacy.R", local = TRUE)
 source("gs_pop_get_count_fast.R", local = TRUE)
 source("parallel_load_gs.R", local = TRUE)
-
+if(!is_tiledb_support())
+{
+  source("pop-add.R", local = TRUE)
+  source("copy_gate.R", local = TRUE)
+  source("filter_to_list.R", local = TRUE)
+  source("scale_gate.R", local = TRUE)
+  source("write.FCS.R", local = TRUE)
+  source("gh_pop_move.R", local = TRUE)
+}
 backend_mode <<- "mem"
 set_default_backend(backend_mode)
 context(paste("backend:", backend_mode))
