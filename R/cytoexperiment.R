40863aaf991d3cbc3f8eb4aee30588f988fbac8f
mikejiang <wjiang2@fhcrc.org>
2020-07-24 12:29:38 -0700

    add cytoexperiment

--- R/cytoexperiment.R
+++ R/cytoexperiment.R
@@ -1,10 +1,28 @@
 #' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #' @export
-setClass("cytoexperiment", contains = c("cytoframe" , "SummarizedExperiment"))
+setClass("cytoexperiment", contains = c("SummarizedExperiment"))
          
-
+#' @importFrom S4Vectors DataFrame SimpleList
+#' @importFrom SummarizedExperiment SummarizedExperiment
+#' @export
 cytoexperiment <- function(cf){
-  new("cytoexperiment"
-      , pointer = cf@pointer
-      )
+  ca <- CytoArray(cf)
+  pd <- pData(parameters(cf))
+  
+  ## somehow can't coerse df to DF
+  # pd$name <- as.character(pd$name)
+  # pd$desc <- as.character(pd$desc)
+  # as(pd, "DataFrame")
+  rd <- DataFrame(#name = pd$name
+                   desc = pd$desc
+                  , range = pd$range
+                  , minRange = pd$minRange
+                  , maxRange = pd$maxRange
+                  , row.names = pd$name #rownames(pd)
+                  )
+  
+  cse <- SummarizedExperiment(assays = SimpleList(intensity = ca)
+                              , rowData = rd)
+  as(cse, "cytoexperiment")
+  
 }
