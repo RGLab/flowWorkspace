4e50cdbf4d573508b87b7337cd49d6d83cad44fa
mikejiang <wjiang2@fhcrc.org>
2020-10-02 20:31:01 -0700

    add test for s3 idx

--- tests/testthat/s3-gs.R
+++ tests/testthat/s3-gs.R
@@ -15,6 +15,17 @@ test_that("save_gs from local to remote",
   {
     tmp <- tempfile()
     convert_backend(gs_dir, tmp)
+    
+    if(use_on_disk_idx())
+    {
+      gs <- load_gs(tmp)
+      gs_convert_idx_to_ondisk(gs)
+      expect_equal(basename(gh_idx_get_uri(gs[[1]])), paste0(sampleNames(gs[[1]]), ".idx"))
+      tmp1 <- tempfile()
+      save_gs(gs, tmp1)
+      unlink(tmp, recursive = TRUE)
+      tmp <- tmp1
+    }
     gs_dir <- tmp
   }
   #load the local gs archive
@@ -27,10 +38,15 @@ test_that("save_gs from local to remote",
   gs_key <- paste0(prefix, "/", guid, ".gs")
   gh_key <- paste0(prefix, "/", sn, ".pb")
   cf_key <- paste0(prefix, "/", sn, ".", get_default_backend())
+  idx_key <- paste0(prefix, "/", sn, ".idx")
   
   keys <- get_bucket_df(url, region = reg)[["Key"]]
   expect_true(is.element(c(gs_key), keys))
   expect_true(is.element(c(gh_key), keys))
+  if(use_on_disk_idx())
+  {
+    expect_equal(sum(grepl(idx_key, keys)), 3)
+  }
   expect_equal(sum(grepl(cf_key, keys)), 21)
   
   if(get_default_backend() == "tile")
@@ -44,7 +60,8 @@ test_that("load_gs from s3",
             gs <- load_gs(url)
             expect_is(gs, "GatingSet")
             expect_true(is_s3_path(cs_get_uri(gs)))
-            
+            if(use_on_disk_idx())
+              expect_true(is_s3_path(gh_idx_get_uri(gs[[1]])))
            })
 test_that("save_gs from s3 to local",
           {
@@ -54,8 +71,13 @@ test_that("save_gs from s3 to local",
             sn <- sampleNames(gs)
             gs_key <- paste0(guid, ".gs")
             gh_key <- paste0(sn, ".pb")
+            idx_key <- paste0(sn, ".idx")
             cf_key <- paste0(sn, ".", get_default_backend())
-            expect_true(setequal(list.files(tmp), c(gs_key, gh_key, cf_key)))
+            
+            res <- c(gs_key, gh_key, cf_key)
+            if(use_on_disk_idx())
+              res <- c(res, idx_key)
+            expect_true(setequal(list.files(tmp), res))
             delete_gs(tmp)
             })
 
