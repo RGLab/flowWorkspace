28cc922acfdf5e4a4e70996609ceee205a817ca0
mikejiang <mike@ozette.ai>
2022-04-24 14:53:12 -0700

    remove unused arguments #376

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -7,6 +7,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // gen_uid
 string gen_uid();
 RcppExport SEXP _flowWorkspace_gen_uid() {
